b30d35c7353b33da94a93b8bbfbacdacbb57e28a
Amigowski <ruediger@weronetzki.de>
2017-05-30 14:47:30 +0200

    Add files via upload

--- Netatmo/module.php
+++ Netatmo/module.php
@@ -13,6 +13,8 @@ class NetatmoSecurity extends IPSModule
         //You cannot use variables here. Just static values.
 		
         $this->RegisterPropertyString("Devicetype", "");
+		$this->RegisterPropertyString("User", "");
+		$this->RegisterPropertyString("Password", "");
 	}
 	
     public function ApplyChanges()
@@ -59,18 +61,69 @@ class NetatmoSecurity extends IPSModule
 		$change = false;
 				
 		$devicetype = $this->ReadPropertyString('Devicetype');
+		$user = $this->ReadPropertyString('User');
+		$password = $this->ReadPropertyString('Password');
 		
 		if ($devicetype == "")
 		{
-			$this->SetStatus(210); // Devicetype darf nicht leer sein
+			$this->SetStatus(201); // Devicetype darf nicht leer sein
 		}
+		else if ($user == "")
+		{
+			$this->SetStatus(202); // Devicetype darf nicht leer sein
+		}
+		else if ($password == "")
+		{
+			$this->SetStatus(203); // Devicetype darf nicht leer sein
+		}
+
+
+
         //$this->EnableAction("EchoTuneInRemote_".$devicenumber);
         //$this->SetStatus(102);
 		
 	}
 
-	 public function MeineErsteEigeneFunktion() {
-            echo $this->InstanceID;
-        }
+	 /************************** Schnittstelle Netatmo *******************************/
+	public function getAccessToken () 
+	{
+    $app_id = '592d2d6c6b0affb65e8b741f';
+    $app_secret = 'lf2bKTh1WpeUkgJe4offqCbHtKzQ48TYG41pYy5ISIlA';
+    $username = 'YOUR_USERNAME';
+    $password = 'YOUR_PASSWORD';
+    $scope = 'read_station read_thermostat write_thermostat'; // all scopes are selected.
+    $token_url = "https://api.netatmo.com/oauth2/token";
+    $postdata = http_build_query(
+        array(
+            'grant_type' => "password",
+            'client_id' => $app_id,
+            'client_secret' => $app_secret,
+            'username' => $username,
+            'password' => $password,
+            'scope' => $scope
+        )
+    );
+
+    $opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/x-www-form-urlencoded',
+        'content' => $postdata
+    )
+    );
+
+    $context  = stream_context_create($opts);
+
+    $response = file_get_contents($token_url, false, $context);
+    $params = null;
+    $params = json_decode($response, true);
+
+    $api_url = "https://api.netatmo.com/api/getuser?access_token="
+    . $params['access_token'];
+
+    $user = json_decode(file_get_contents($api_url);
+    echo("It worked. Hello <".$user->body->mail.">\n");
+
+    }
 } 
 ?>

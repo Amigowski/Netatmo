a6bbef3610b23ac9dcd7ae60841bf87517465797
Amigowski <ruediger@weronetzki.de>
2017-06-03 14:06:54 +0200

    Add files via upload

--- Netatmo/module.php
+++ Netatmo/module.php
@@ -335,7 +335,7 @@ IPS_LogMessage("Netatmo WebHook RAW", file_get_contents("php://input"));
         $requete = @file_get_contents($api_url);
         $jsonDatas = json_decode($requete,true);
          if ($jsonDatas['status']== 'ok') {
-			IPS_LogMessage($this->logSource, "Unhook successfull ".$endpoint);
+			IPS_LogMessage($this->logSource, "Unhook successfull ");
 			return true;			
 		}
         IPS_LogMessage($this->logSource, "Unhook status:".$jsonDatas['status']);

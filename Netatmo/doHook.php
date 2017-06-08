0d569c7a6999592985bde26da1f353cc7a5e50f9
Amigowski <ruediger@weronetzki.de>
2017-06-08 13:28:13 +0200

    Add files via upload

--- Netatmo/doHook.php
+++ Netatmo/doHook.php
@@ -1,6 +1,49 @@
 <?
 function doTheHook ($jsondata) {
     IPS_LogMessage("Netatmo DoTheHook RAW", $jsondata);
-} 
 
+    $notif = json_decode($jsonData, TRUE);
+    
+    $message = '';
+    $eventype = '';
+
+
+    if(isset($notif['message']))
+    {
+        $message = $notif['message'];
+    }
+    
+    if(isset($notif['event_type']))
+    {
+        $eventype = $notif['event_type'];
+    }
+    
+    if(isset($notif['camera_id']))
+    {
+        SetValueString(get_VID_CamId(),$notif['camera_id']);
+    }
+    
+    if(isset($notif['home_id']))
+    {
+        SetValueString(get_VID_HomeId(),$notif['home_id']);
+    }
+    
+    if(isset($notif['home_name']))
+    {
+        SetValueString(get_VID_Home(),$notif['home_name']);
+    }
+}
+
+function getParent () {
+    return IPS_GetParent($_IPS['SELF']);
+}
+function get_VID_HomeId() {
+    return IPS_GetVariableIDByName ( 'HomeId', getParent());
+}
+function get_VID_HomeName() {
+    return IPS_GetVariableIDByName ( 'Home', getParent());
+}
+function get_VID_CamId() {
+    return IPS_GetVariableIDByName ( 'CamId', getParent());
+}
 ?>
